61f6cbc65a413ccb1cbb82c54e2d33c474350a4b
Muhammadkhuja Kamoliddinzoda <mkamoliddinzoda2004@gmail.com>
2024-04-16 02:12:26 +0300

    added 'save', 'open', 'new'

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTextEdit
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTextEdit, QAction, QFileDialog
 
 
 class Main(QMainWindow):
 
     def __init__(self):
         super(QMainWindow, self).__init__()
+
+        self.filename = ""
 
         self.init_ui()
 
@@ -25,7 +27,28 @@ class Main(QMainWindow):
         self.setWindowTitle("Writer")
 
     def init_tool_bar(self):
+        self.new_action = QAction(QIcon("icons/new.png"), "New", self)
+        self.new_action.setStatusTip("Create a new document from scratch.")
+        self.new_action.setShortcut("Ctrl+N")
+        self.new_action.triggered.connect(self.new)
+
+        self.open_action = QAction(QIcon("icons/open.png"), "Open file", self)
+        self.open_action.setStatusTip("Open existing document")
+        self.open_action.setShortcut("Ctrl+O")
+        self.open_action.triggered.connect(self.open)
+
+        self.save_action = QAction(QIcon("icons/save.png"), "Save", self)
+        self.save_action.setStatusTip("Save document")
+        self.save_action.setShortcut("Ctrl+S")
+        self.save_action.triggered.connect(self.save)
+
         self.toolbar = self.addToolBar("Options")
+
+        self.toolbar.addAction(self.new_action)
+        self.toolbar.addAction(self.open_action)
+        self.toolbar.addAction(self.save_action)
+
+        self.toolbar.addSeparator()
 
         self.addToolBarBreak()
 
@@ -38,6 +61,33 @@ class Main(QMainWindow):
         file = menubar.addMenu("File")
         edit = menubar.addMenu("Edit")
         view = menubar.addMenu("View")
+        file.addAction(self.new_action)
+        file.addAction(self.open_action)
+        file.addAction(self.save_action)
+
+    def new(self):
+        spawn = Main(self)
+        spawn.show()
+
+    def open(self):
+        self.filename = QFileDialog.getOpenFileName(self, 'Open File', ".", "(*.writer)")
+
+        if self.filename:
+            with open(self.filename, "rt") as file:
+                self.text.setText(file.read())
+
+    def save(self):
+
+        if not self.filename:
+            self.filename = QFileDialog.getSaveFileName(self, 'Save File')
+
+        if not self.filename.endswith(".writer"):
+            self.filename += ".writer"
+
+        with open(self.filename, "wt") as file:
+            file.write(self.text.toHtml())
+
+
 
 
 def main():
