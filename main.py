97de28b38da5df760119748173e367fe75b8c725
Muhammadkhuja Kamoliddinzoda <mkamoliddinzoda2004@gmail.com>
2024-04-16 01:27:08 +0300

    added tools bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTextEdit
 
 
 class Main(QMainWindow):
@@ -8,12 +8,39 @@ class Main(QMainWindow):
     def __init__(self):
         super(QMainWindow, self).__init__()
 
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
+    def init_ui(self):
+        self.text = QTextEdit(self)
+        self.setCentralWidget(self.text)
+
+        self.init_tool_bar()
+        self.initFormatbar()
+        self.init_menu_bar()
+
+        # Initialize a statusbar for the window
+        self.statusbar = self.statusBar()
+
+        # x and y coordinates on the screen, width, height
         self.setGeometry(100, 100, 1030, 800)
 
         self.setWindowTitle("Writer")
+
+    def init_tool_bar(self):
+        self.toolbar = self.addToolBar("Options")
+
+        # Makes the next toolbar appear underneath this one
+        self.addToolBarBreak()
+
+    def initFormatbar(self):
+        self.formatbar = self.addToolBar("Format")
+
+    def init_menu_bar(self):
+        menubar = self.menuBar()
+
+        file = menubar.addMenu("File")
+        edit = menubar.addMenu("Edit")
+        view = menubar.addMenu("View")
 
 
 def main():
